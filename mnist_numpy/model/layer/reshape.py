12cded66f97294835a63baece79c5a1917e68737
Moye <moyeodiase@hotmail.co.uk>
2025-05-13 07:36:27 +0100

    WIP: Add convolution 'block'

--- mnist_numpy/model/layer/reshape.py
+++ mnist_numpy/model/layer/reshape.py
@@ -1,5 +1,8 @@
+import operator
+from functools import reduce
+
 from mnist_numpy.model.layer.base import Hidden
-from mnist_numpy.protos import Activations, D
+from mnist_numpy.protos import Activations, D, Dimensions
 
 
 class Reshape(Hidden):
@@ -7,6 +10,20 @@ class Reshape(Hidden):
         return input_activations.reshape(
             input_activations.shape[0], *self.output_dimensions
         )
+
+    def _backward_prop(self, *, dZ: D[Activations]) -> D[Activations]:
+        return dZ.reshape(dZ.shape[0], *self.input_dimensions)
+
+
+class Flatten(Reshape):
+    def __init__(self, *, input_dimensions: Dimensions):
+        super().__init__(
+            input_dimensions=input_dimensions,
+            output_dimensions=(reduce(operator.mul, input_dimensions, 1),),
+        )
+
+    def _forward_prop(self, *, input_activations: Activations) -> Activations:
+        return input_activations.reshape(input_activations.shape[0], -1)
 
     def _backward_prop(self, *, dZ: D[Activations]) -> D[Activations]:
         return dZ.reshape(dZ.shape[0], *self.input_dimensions)
